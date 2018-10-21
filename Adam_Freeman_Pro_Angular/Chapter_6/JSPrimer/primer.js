9da4fe5a7246834595ab83391a236b71f8f92d9f
Aionoff <Aionoff62@mail.ru>
2018-10-21 15:23:08 +0300

    Greeting to comsole has been implemented

--- Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.js
+++ Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.js
@@ -1,2 +1,5 @@
-console.log("Hello");
-console.log("apples");
+var myData = new Object();
+myData.name = "Adam";
+myData.weather = "sunny";
+console.log("Hello " + myData.name + ".");
+console.log("Today is " + myData.weather + ".");
--- Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
+++ Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
@@ -1,2 +1,6 @@
-console.log("Hello");
-console.log("apples");
+let myData = new Object();
+myData.name = "Adam";
+myData.weather = "sunny";
+
+console.log("Hello " + myData.name + ".");
+console.log("Today is " + myData.weather + ".");
