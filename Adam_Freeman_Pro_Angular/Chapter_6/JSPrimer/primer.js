328c0d57149faf6520081278d78deed4856f5d0f
Aionoff <Aionoff62@mail.ru>
2018-10-21 15:28:21 +0300

    myData has been rebuilded like variable

--- Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.js
+++ Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.js
@@ -1,5 +1,9 @@
-var myData = new Object();
-myData.name = "Adam";
-myData.weather = "sunny";
-console.log("Hello " + myData.name + ".");
-console.log("Today is " + myData.weather + ".");
+var myData = {
+    name: "Adam",
+    weather: "sunny",
+    printMessages: function () {
+        console.log("Hello " + this.name + ". ");
+        console.log("Today is " + this.weather + ".");
+    }
+};
+myData.printMessages();
--- Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
+++ Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
@@ -1,6 +1,11 @@
-let myData = new Object();
-myData.name = "Adam";
-myData.weather = "sunny";
+let myData = {
+  name: "Adam",
+  weather: "sunny",
 
-console.log("Hello " + myData.name + ".");
-console.log("Today is " + myData.weather + ".");
+  printMessages: function(){
+    console.log("Hello " + this.name + ". ");
+    console.log("Today is " + this.weather + ".");
+  }
+};
+
+myData.printMessages();
