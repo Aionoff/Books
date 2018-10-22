b5e23605848edbfa2de123e8f08177cb2dd687cf
Aionoff <Aionoff62@mail.ru>
2018-10-22 10:45:37 +0300

    F to C converter has been implemented

--- Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
+++ Adam_Freeman_Pro_Angular/Chapter_6/JSPrimer/primer.ts
@@ -1,10 +1,14 @@
 import {Name, WeatherLocation} from "./modules/NameAndWeather";
 import {Name as OtherName} from "./modules/DuplicateName";
+import { TempConverter } from "./tempConverter";
 
 let name = new Name("Adam", "Freeman");
 let loc = new WeatherLocation("raining", "London");
 let other = new OtherName();
 
+let cTemp = TempConverter.convertFtoC(38);
+
 console.log(name.nameMessage);
 console.log(loc.weatherMessage);
 console.log(other.message);
+console.log(cTemp);
